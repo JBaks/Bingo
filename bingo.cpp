d47db1b4edc071fe9df610310e064353eeb9779b
JBaks <50462947+JBaks@users.noreply.github.com>
2019-05-20 23:10:31 +0200

    Update bingo.cpp

--- bingo.cpp
+++ bingo.cpp
@@ -5,7 +5,7 @@
 int main()
 {
 	int niz[48]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48};
-	int i,brojevi[6];
+	int i,brojevi[6],j,pom=0;
 	
 	printf("Unesite 6 brojeva po vasem izboru:\n");
 	for(i=0;i<6;i++)
@@ -36,8 +36,9 @@ int main()
 		niz[i]=niz[j];
 		niz[j]=pom;
 		}
-		for(i=0;i<48;i++)
+		for(i=0;i<35;i++)
 		{
 			printf("%d ",niz[i]);
 		}
+			
 }
