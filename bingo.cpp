d303f3fd506fa5381d25a6222d07b4ddc87c79d4
Vinko-Tino <50461476+zlopy99@users.noreply.github.com>
2019-05-23 15:59:15 +0200

    Update bingo.cpp

--- bingo.cpp
+++ bingo.cpp
@@ -18,32 +18,41 @@ void delay(int number_of_seconds)
 int main()
 {
 int niz[48]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48};
-int iznos[31]={20000,10000,7500,5000,2500,1000,500,300,200,150,100,90,80,70,60,50,40,30,25,20,15,10,9,8,7,6,5,4,3,2,1};	
-int i,brojevi[6],j,pom=0,pobjeda=0,br_p,obicno;
-	
-printf("Unesite 6 brojeva po vasem izboru:\n");	
-for(i=0;i<6;i++)
-{
-	scanf("%d",&brojevi[i]);
-	if(brojevi[i]<1 || brojevi[i]>48 || brojevi[i]==brojevi[i-1]||brojevi[i]==brojevi[i-2]||brojevi[i]==brojevi[i-3]||brojevi[i]==brojevi[i-4]||brojevi[i]==brojevi[i-5])
-	{
-		printf("Unesite ponovo:\n");
-		i--;
-	}
-		//Jos jedan primjer ako imamo 2 ista broja!
-		/*else
-        	{
-            	for(int k=0;k<i;k++){   
-                	if(brojevi[i]==brojevi[k]){
-                    		printf("Unesite ponovo:\n");
-                    		i--;
-                	}
-            	}
-		}*/
+int iznos[31]={20000,10000,7500,5000,2500,1000,500,300,200,150,100,90,80,70,60,50,40,30,25,20,15,10,9,8,7,6,5,4,3,2,1};
+int i,brojevi[6],j,pom=0,pobjeda=0,br_p,obicno,pozitiva=1;
 
+printf("Izaberite jednu od dvije opcije:\n\n");
+printf("Pisat svoje brojeve--Stisnite 1.\n");
+printf("Slucajni listic--Stisnite 2.\n\n");
 
+while(pozitiva==1){
+    switch(getch()){
+    case '1':
+        printf("Unesite 6 brojeva po vasem izboru:\n");
+        for(i=0;i<6;i++)
+        {
+            scanf("%d",&brojevi[i]);
+            if(brojevi[i]<1 || brojevi[i]>48 || brojevi[i]==brojevi[i-1]||brojevi[i]==brojevi[i-2]||brojevi[i]==brojevi[i-3]||brojevi[i]==brojevi[i-4]||brojevi[i]==brojevi[i-5])
+            {
+                printf("Unesite ponovo:\n");
+                i--;
+            }
+        }
+        pozitiva=0;
+        break;
+    case '2':
+        srand(time(NULL));
+        for(i=0;i<6;i++){
+            j=(rand()%47)+1;
+            brojevi[i]=j;
+        }
+        pozitiva=0;
+        break;
+    default:
+        printf("Pritisnite 1. ili 2.\n\n");
+    }
 }
-	
+
 printf("Vasi brojevi su:\n");
 for(i=0;i<6;i++)
 {
@@ -55,22 +64,22 @@ printf("\n");
 srand(time(NULL));
 for(i=0;i<48;i++)
 {
-	j=(rand()%48);
+	j=(rand()%47)+1;
 
 	pom=niz[i];
 	niz[i]=niz[j];
 	niz[j]=pom;
-		
+
 	for(int br=0;br<6;br++)
 	{
             	if((niz[i]==brojevi[br])&&(i<5))
 		{
-                	pobjeda++;	
+                	pobjeda++;
             	}
         }
 }
-		
-for(i=0;i<35;i++)				
+
+for(i=0;i<35;i++)
 {
 	obicno=1;
 
@@ -107,8 +116,8 @@ delay(1);
 }
 printf("\n");
 printf("\n");
-		
-		
+
+
 if(pobjeda==5)
 {
 printf("Cestitamo!! Osvojili ste 20 000 KM \n");
@@ -145,5 +154,5 @@ else
     }
     printf("\n");
 }
-		
+delay(10);
 }
